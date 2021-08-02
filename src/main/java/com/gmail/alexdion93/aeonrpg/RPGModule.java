94761547c21af8d1efacb3ddf30ad651e0b86151
Alex Dion <alexdion93@gmail.com>
2021-08-01 20:06:27 -0400

    Made code drier and added overloaded methods

--- src/main/java/com/gmail/alexdion93/aeonrpg/RPGModule.java
+++ src/main/java/com/gmail/alexdion93/aeonrpg/RPGModule.java
@@ -1,5 +1,6 @@
 package com.gmail.alexdion93.aeonrpg;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
@@ -93,21 +94,37 @@ public abstract class RPGModule extends JavaPlugin {
    * @param types One or more rpg attributes
    */
   protected void registerAttributes(RPGAttributeType... types) {
-    
     GenericRPGTypeManager<RPGAttributeType> manager = aeonrpg.getRPGDataTypeManager().getAttributeManager();
-    PluginManager pm = Bukkit.getPluginManager();
-    Logger log = getLogger();
-    
-    for(RPGAttributeType type : types) {
-      try {
-          manager.put(type);
-          if (type instanceof Listener) { pm.registerEvents((Listener) type, this); }
-          if (type instanceof RPGRecipeModifier) { ((RPGRecipeModifier) type).modifyRecipes(); }
-      }
-      catch (Exception e) {
-          log.warning(String.format("Failed to register %s. %s", type.getDisplayName(), e.toString()));
-      } 
+    for(RPGAttributeType type : types) { registerData(manager, type); }
+  }
+  
+  /**
+   * Attempts to register one or more RPGAttributes to the appropriate manager
+   * and performs certain operations based on the interfaces included:
+   * • If the type is a listener it will automatically register it to this module
+   * • If the type modifies a vanilla recipe it will be applied to them
+   * @param types A collection of attributes
+   */
+  protected void registerAttributes(List<RPGAttributeType> types) {
+    GenericRPGTypeManager<RPGAttributeType> manager = aeonrpg.getRPGDataTypeManager().getAttributeManager();
+    for(RPGAttributeType type : types) { registerData(manager, type); }
+  }
+  
+  /**
+   * Registers a single type of data to the appropriate manager type
+   * @param manager The manager we're registering to.
+   * @param type The being registered.
+   */
+  private <T extends RPGDataType> void registerData(GenericRPGTypeManager<T> manager, T type) {
+    try {
+      manager.put(type);
+      if (type instanceof Listener) { Bukkit.getPluginManager().registerEvents((Listener) type, this); }
+      if (type instanceof RPGRecipeModifier) { ((RPGRecipeModifier) type).modifyRecipes(); }
     }
+    catch (Exception e) {
+      getLogger().warning(String.format("Failed to register %s. %s", type.getDisplayName(), e.toString()));
+    }
+    
   }
   
   /**
@@ -118,23 +135,22 @@ public abstract class RPGModule extends JavaPlugin {
    * @param types One or more rpg data types
    */
   protected void registerEnchantments(RPGEnchantmentType... types) {
-    
     GenericRPGTypeManager<RPGEnchantmentType> manager = aeonrpg.getRPGDataTypeManager().getEnchantmentManager();
-    PluginManager pm = Bukkit.getPluginManager();
-    Logger log = getLogger();
-    
-    for(RPGEnchantmentType type : types) {
-      try {
-          manager.put(type);
-          if (type instanceof Listener) { pm.registerEvents((Listener) type, this); }
-          if (type instanceof RPGRecipeModifier) { ((RPGRecipeModifier) type).modifyRecipes(); }
-      }
-      catch (Exception e) {
-          log.warning(String.format("Failed to register %s. %s", type.getDisplayName(), e.toString()));
-      } 
-    }
+    for(RPGEnchantmentType type : types) {registerData(manager, type);}
   }
   
+  /**
+   * Attempts to register one or more RPGEnchantmentTypes to the appropriate manager
+   * and performs certain operations based on the interfaces included:
+   * • If the type is a listener it will automatically register it to this module
+   * • If the type modifies a vanilla recipe it will be applied to them
+   * @param types A collection of enchantments
+   */
+  protected void registerEnchantments(List<RPGEnchantmentType> types) {
+    GenericRPGTypeManager<RPGEnchantmentType> manager = aeonrpg.getRPGDataTypeManager().getEnchantmentManager();
+    for(RPGEnchantmentType type : types) {registerData(manager, type);}
+  }
+ 
   /**
    * Attempts to register one or more RPGDataTypes to the appropriate manager
    * and performs certain operations based on the interfaces included:
@@ -143,21 +159,20 @@ public abstract class RPGModule extends JavaPlugin {
    * @param types One or more rpg data types
    */
   protected void registerGenerics(RPGDataType... types) {
-    
     GenericRPGTypeManager<RPGDataType> manager = aeonrpg.getRPGDataTypeManager().getGenericManager();
-    PluginManager pm = Bukkit.getPluginManager();
-    Logger log = getLogger();
-    
-    for(RPGDataType type : types) {
-      try {
-          manager.put(type);
-          if (type instanceof Listener) { pm.registerEvents((Listener) type, this); }
-          if (type instanceof RPGRecipeModifier) { ((RPGRecipeModifier) type).modifyRecipes(); }
-      }
-      catch (Exception e) {
-          log.warning(String.format("Failed to register %s. %s", type.getDisplayName(), e.toString()));
-      } 
-    }
+    for(RPGDataType type : types) {registerData(manager, type);}
+  }
+  
+  /**
+   * Attempts to register one or more RPGDataTypes to the appropriate manager
+   * and performs certain operations based on the interfaces included:
+   * • If the type is a listener it will automatically register it to this module
+   * • If the type modifies a vanilla recipe it will be applied to them
+   * @param types A collection of generics
+   */
+  protected void registerGenerics(List<RPGDataType> types) {
+    GenericRPGTypeManager<RPGDataType> manager = aeonrpg.getRPGDataTypeManager().getGenericManager();
+    for(RPGDataType type : types) {registerData(manager, type);}
   }
   
   /**
@@ -167,22 +182,21 @@ public abstract class RPGModule extends JavaPlugin {
    * • If the type modifies a vanilla recipe it will be applied to them
    * @param types One or more rpg data types
    */
-  protected void registerEnchantments(RPGPotionEffectType... types) {
-    
+  protected void registerPotionEffects(RPGPotionEffectType... types) {
     GenericRPGTypeManager<RPGPotionEffectType> manager = aeonrpg.getRPGDataTypeManager().getPotionEffectManager();
-    PluginManager pm = Bukkit.getPluginManager();
-    Logger log = getLogger();
-    
-    for(RPGPotionEffectType type : types) {
-      try {
-          manager.put(type);
-          if (type instanceof Listener) { pm.registerEvents((Listener) type, this); }
-          if (type instanceof RPGRecipeModifier) { ((RPGRecipeModifier) type).modifyRecipes(); }
-      }
-      catch (Exception e) {
-          log.warning(String.format("Failed to register %s. %s", type.getDisplayName(), e.toString()));
-      } 
-    }
+    for(RPGPotionEffectType type : types) {registerData(manager, type);}
+  }
+  
+  /**
+   * Attempts to register one or more RPGPotionEffectTypes to the appropriate manager
+   * and performs certain operations based on the interfaces included:
+   * • If the type is a listener it will automatically register it to this module
+   * • If the type modifies a vanilla recipe it will be applied to them
+   * @param types A collection of attributes
+   */
+  protected void registerPotionEffects(List<RPGPotionEffectType> types) {
+    GenericRPGTypeManager<RPGPotionEffectType> manager = aeonrpg.getRPGDataTypeManager().getPotionEffectManager();
+    for(RPGPotionEffectType type : types) {registerData(manager, type);}
   }
   
   /**
